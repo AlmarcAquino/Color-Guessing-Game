14cea2eb4a66afe0fe611dd2b14e6c245d52f260
almarcaquino <almarcfaquino@gmail.com>
2022-08-13 13:48:27 -0700

    Very Messy (update?) WIP

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ let colorsContainerEl = document.querySelector('.colors__container');
 let colorBlockEl = [...document.querySelectorAll('.color__block')];
 let numberOfBlocks = colorBlockEl.length;
 let lastBlock = colorBlockEl[numberOfBlocks - 1];
+let score = 0;
 
 let updateBlocks = () => {
-  let colorBlockEl = [...document.querySelectorAll('.color__block')];
+  colorBlockEl = [...document.querySelectorAll('.color__block')];
   lastBlock = colorBlockEl[numberOfBlocks - 1];
   return colorBlockEl;
 };
@@ -29,18 +30,50 @@ controlButtons.forEach((button) => {
   });
 });
 
+// Event listeners for when a block is clicked
+colorBlockEl.forEach((block) => {
+  block.addEventListener('click', () => {
+    if (block.classList.contains(`correct`)) {
+      console.log('that is the correct block');
+      block.classList.remove('correct');
+      score++;
+      shuffleBlockColors();
+    } else {
+      console.log('wrong block');
+      block.classList.remove('correct');
+      score = 0;
+      shuffleBlockColors();
+    }
+  });
+});
+
 // Generate random RGB color
 function newRandomColor() {
   let rgbArray = [];
   for (let i = 0; i < 3; i++) {
     rgbArray.push(Math.floor(Math.random() * 256) + 1);
   }
-  colorValueEl.innerHTML = `RGB (${rgbArray[0]}, ${rgbArray[1]}, ${rgbArray[2]} )`;
-  // needs to assign one random block to the same color
-  return;
+  return rgbArray;
 }
 // call on start
 newRandomColor();
+
+// Shuffle block colors
+function shuffleBlockColors() {
+  // Assign a random color to each block
+  console.log(colorBlockEl);
+  colorBlockEl.forEach(block, () => {
+    let rgbValues = newRandomColor();
+    block.style.backgroundColor = `rgb(${rgbValues[0]}, ${rgbValues[1]}, ${rgbValues[2]})`;
+  });
+
+  let correctBlockRGB = newRandomColor();
+  colorValueEl.innerHTML = `RGB (${correctBlockRGB[0]}, ${correctBlockRGB[1]}, ${correctBlockRGB[2]} )`;
+  // assign random block 'correct' class and bgc
+  let randomBlock = colorBlockEl[Math.floor(Math.random() * numberOfBlocks)];
+  randomBlock.classList.add(`correct`);
+  randomBlock.style.backgroundColor = `rgb(${correctBlockRGB[0]}, ${correctBlockRGB[1]}, ${correctBlockRGB[2]})`;
+}
 
 // Remove one color block (min 2)
 function removeColorBlock() {
@@ -58,8 +91,26 @@ function addColorBlock() {
   if (numberOfBlocks === 6) {
     return;
   }
+
   numberOfBlocks += 1;
-  colorsContainerEl.innerHTML += `<div class="color__block block-${numberOfBlocks}"></div>`;
+
+  let newDiv = document.createElement('div');
+  newDiv.classList.add(`color__block`, `block-${numberOfBlocks}`);
+  newDiv.addEventListener('click', () => {
+    if (newDiv.classList.contains(`correct`)) {
+      console.log('that is the correct block');
+      block.classList.remove('correct');
+      score++;
+      shuffleBlockColors();
+    } else {
+      console.log('wrong block');
+      block.classList.remove('correct');
+      score = 0;
+      shuffleBlockColors();
+    }
+  });
+
+  colorsContainerEl.appendChild(newDiv);
   updateBlocks();
   return;
 }
